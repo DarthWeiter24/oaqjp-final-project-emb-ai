8468ef6547b289474167f6d6f229806491dfff69
DarthWeiter24 <patilanish2401+github@gmail.com>
2025-02-17 14:54:22 -0500

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    '''This method analyses text and detects the emotion
+    '''
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post( url, json = myobj, headers = header)
+    formatted_reponse = json.loads(response.text)
+    emotion_values = formatted_reponse['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = max(emotion_values,key=emotion_values.get)
+    
+    result = {
+        'anger': emotion_values['anger'],
+        'disgust': emotion_values['disgust'],
+        'fear': emotion_values['fear'],
+        'joy': emotion_values['joy'],
+        'sadness': emotion_values['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    return result
